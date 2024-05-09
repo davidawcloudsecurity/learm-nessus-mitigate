38128aaf028ca159c2861619ebb7d4c45a7ab1e3
test <test@test.com>
2024-05-09 23:07:07 +0800

    Update repo

---
+++ 4.1.3.12_configure_audit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the audit rules file
+AUDIT_RULES_FILE="/etc/audit/rules.d/50-login.rules"
+
+# Add audit rules to the file
+printf '-w /var/log/lastlog -p wa -k logins
+-w /var/run/faillock -p wa -k logins
+' | sudo tee -a "$AUDIT_RULES_FILE" > /dev/null
+
+# Merge and load the rules into the active configuration
+sudo augenrules --load
+
+# Check if a reboot is required
+if [[ $(auditctl -s | grep 'enabled') =~ '2' ]]; then
+    printf 'Reboot required to load rules\n'
+fi
+
