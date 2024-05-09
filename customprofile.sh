0d4614f93ffc44764816152ec3a2d9a3a2291f6b
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-05-09 15:06:01 +0800

    Update customprofile.sh

--- customprofile.sh
+++ customprofile.sh
@@ -4,8 +4,11 @@
 read -p "Enter the custom profile name: " profile_name
 
 # Construct the authselect command with the custom pro:wfile name and default options
+authcreate_command="authselect create-profile $profile_name -b sssd --symlink-meta
 authselect_command="authselect select custom/$profile_name with-sudo with-faillock without-nullok"
 
 # Execute the authselect command
+echo "Executing: $authcreate_command"
+$authcreate_command
 echo "Executing: $authselect_command"
 $authselect_command
