58dc0ee273568aee64ed853245de3ff2bab04513
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-05-13 09:34:15 +0800

    Update 3.1.3_disabletipc.sh

--- 3.1.3_disabletipc.sh
+++ 3.1.3_disabletipc.sh
@@ -3,15 +3,15 @@
 {
    l_mname='tipc' # set module name
    # Check if the module exists on the system
-   if [ -z "$(modprobe -n -v "$l_mname" 2>&1 | grep -Pi -- 'modprobe: FATAL: Module '$l_mname' not found in directory')" ]; then
+   if [ -z "$(modprobe -n -v "$l_mname" 2>&1 | grep -Pi -- 'h*modprobe:h+FATAL:h+Moduleh+'"$l_mname"'.+noth+foundh+inh+directory')" ]; then
       # Remediate loadable
       l_loadable="$(modprobe -n -v "$l_mname")"
-      if [ "$(echo "$l_loadable" | wc -l)" -gt '1' ]; then
-         l_loadable="$(echo "$l_loadable" | grep -P -- '(^install| '$l_mname')')"
+      if [[ $(wc -l <<< "$l_loadable") -gt 1 ]]; then
+         l_loadable="$(grep -P '(^h*install|b'"$l_mname"'b)' <<< "$l_loadable")"
       fi
-      if ! echo "$l_loadable" | grep -Pq -- '^install /bin/(true|false)'; then
+      if ! grep -Pq '^h*install /bin/(true|false)' <<< "$l_loadable"; then
          echo -e " - setting module: $l_mname to be not loadable"
-         echo -e "install $l_mname /bin/false" >> "/etc/modprobe.d/$l_mname.conf"
+         echo -e "install $l_mname /bin/false" >> /etc/modprobe.d/"$l_mname".conf
       fi
       # Remediate loaded
       if lsmod | grep "$l_mname" > /dev/null 2>&1; then
@@ -19,12 +19,11 @@
          modprobe -r "$l_mname"
       fi
       # Remediate deny list
-      if ! modprobe --showconfig | grep -Pq -- "^blacklist $l_mname"; then
+      if ! modprobe --showconfig | grep -Pq '^h*blacklist +'"$l_mname"'\b'; then
          echo -e " - deny listing $l_mname"
-         echo -e "blacklist $l_mname" >> "/etc/modprobe.d/$l_mname.conf"
+         echo -e "blacklist $l_mname" >> /etc/modprobe.d/"$l_mname".conf
       fi
    else
       echo -e " - Nothing to remediate\n - Module $l_mname doesn't exist on the system"
    fi
 }
-
